91138bdd662adc3f4cb2d551a10f8a5536750d79
Elliot Silberwasser <73140437+esilberw@users.noreply.github.com>
2023-02-20 21:55:17 +0100

    Add functions to manipulate the DB
    AddUser
    UpdateUsername
    UpdatePassword
    getLearderboard
    getScore
    removeScore
    
    all functions work

--- src/Driver.java
+++ src/Driver.java
@@ -11,36 +11,10 @@ public class Driver {
         try { // if the connection failed
 
             Connection connection = DriverManager.getConnection(url, user, password);
-
-            boolean checkFlag = getScore("Elliot", connection);
-
-            if (!checkFlag){
-                System.out.println("User is already register");
-            }
-
-
-           //Statement statement = connection.createStatement();
-
-            //System.out.println("Table des users:");
-            // resultSet is a structure that catch the answers from the database.
-            //String queryUser = "SELECT * FROM users";
-
-            //ResultSet resultSet = statement.executeQuery(queryUser);
-
-            // .next() is a boolean value. True when there stay one more data to show.
-            //while (resultSet.next()){
-            //    System.out.println(resultSet.getString("password"));
-            //}
-
-
-            //System.out.println("Table des scores:");
-            //ResultSet rs = statement.executeQuery("SELECT * FROM score");
-
-            //while (rs.next()){
-            //    System.out.println(rs.getString("username"));
-            //    System.out.println(rs.getString("score"));
-            //}
-
+            updatePassword("Elliot", "6666", connection);
+            updateUsername("FNIERI", 2, connection);
+            //addUser("Nieri", "1111", connection);
+            //getScore("Theo", connection);
 
         }
 
@@ -49,30 +23,74 @@ public class Driver {
         }
     }
 
-    public static boolean addUser(String username, String password, Connection connection) throws SQLException {
+
+    public static String stringToSql(String string) {
+        // function to add simple quote to a Java String for MySql strings.
+        String sqlString = "'" + string + "'";
+        return sqlString;
+    }
+
+    public static void updatePassword(String username, String password, Connection connection) throws SQLException{
         Statement statement = connection.createStatement();
-        String addUserQuery = "SELECT * FROM users WHERE username='Elliot'";
+        String updatePasswordQuery = "UPDATE users SET password =" + stringToSql(password) + "WHERE username =" + stringToSql(username);
+        statement.executeUpdate(updatePasswordQuery);
+    }
 
-        ResultSet rs = statement.executeQuery(addUserQuery);
+    public static void updateUsername(String username, Integer userid, Connection connection) throws SQLException{
+        Statement statement = connection.createStatement();
+        String updateUsername = "UPDATE users SET username =" + stringToSql(username) + "WHERE password =" + userid;
+        statement.executeUpdate(updateUsername);
+    }
 
-        while (rs.next()){
-            System.out.println(rs.getString("username"));
-            System.out.println(rs.getString("password"));
+    public static void addUser(String username, String password, Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        String addUserQuery = "INSERT INTO `blockchainappdb`.`users` (`username`, `password`) VALUES (" + stringToSql(username) + ", "+ stringToSql(password) + ")" + ";";
+        System.out.println(addUserQuery);
+
+        try {
+            statement.executeUpdate(addUserQuery);
         }
-        return true;
+
+        catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
     }
 
-    public static boolean getScore(String username, Connection connection) throws SQLException {
+    public static void getScore(String username, Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
-        String addUserQuery = "SELECT * FROM score WHERE username="+ username;
+        String getScoreQuery = "SELECT score FROM scores WHERE username =" + stringToSql(username);
 
-        ResultSet rs = statement.executeQuery(addUserQuery);
+        ResultSet rs = statement.executeQuery(getScoreQuery);
 
         while (rs.next()){
             System.out.println(rs.getString("score"));
         }
-        return true;
+    }
 
+    public static void deleteScore(String username, Connection connection) throws  SQLException{
+        Statement statement = connection.createStatement();
+        String deleteScoreQuery = "DELETE FROM scores WHERE username =" + stringToSql(username);
+
+        try {
+            statement.executeUpdate(deleteScoreQuery);
+        }
+
+        catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+    public static void getLeaderboard(Connection connection) throws  SQLException{
+        Statement statement = connection.createStatement();
+        String getLeaderBoardQuery = "SELECT score FROM scores ORDER BY score LIMIT 10";
+
+        ResultSet rs = statement.executeQuery(getLeaderBoardQuery);
+
+        while (rs.next()){
+            System.out.println(rs.getString("score"));
+        }
     }
 }
