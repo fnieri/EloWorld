391238c34ee912c17669a433d604a145356ccc42
emerian320 <emile.merian@ulb.be>
2023-02-24 12:35:54 +0100

    innit Users Classes to link server to blockchain

--- src/Referee.java
+++ src/Referee.java
@@ -12,6 +12,25 @@ public class Referee extends User implements Serializable {
         return score;
     }
 
+    /**
+     * Create an entry for a played game and store it in a json temp file.
+     */
+    public void createEntry(String player1, String player2, Boolean player1Wins) {
+        // fetch the 2 names in DB to make sure they properly exist
+
+        // Check the current leaderboard in the server to get their current elo and calculate their new elo
+        // (the function should already be implemented in another class)
+    }
+
+    /**
+     * Prompts the referee to choose which entries to add to his next block amongst the one stored in the temp file.
+     * Then fetches the blockchains/referee scores available and prompts the referee to choose which blockchain to
+     * contribute on. Create a copy of the chosen blockchain, overwriting his own, then add his new block to it.
+     */
+    public void createBlock() {
+
+    }
+
     @Override
     public JSONObject asJson() {
         JSONObject refereeJson =  new JSONObject();
@@ -19,5 +38,8 @@ public class Referee extends User implements Serializable {
         return refereeJson;
     }
 
+    // TODO cette fonction envoie au superuser une requete de demotion
+    public void requestDemotion() {
 
+    }
 }
--- src/User.java
+++ src/User.java
@@ -6,17 +6,16 @@ public class User {
     String publicKey;
     int ELO;
 
-    void register(){}
-
-    void logIn(){}
-
-    void calculateElo(){}
-
     public String getPublicKey() {
         return publicKey;
     }
 
-    public int getELO() {
-        return ELO;
+    // TODO cette fonction doit demander au serveur le elo du joueur par rapport au classement stocké dans le serveur.
+    //  Si le serveur ne trouve pas ce joueur, on renvoie le elo par défaut.
+    public int getELO() {return 0;}
+
+    // TODO cette fonction envoie au superUser une requete de promotion
+    public void requestPromotion() {
+
     }
 }
