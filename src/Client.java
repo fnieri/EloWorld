5d47c59ded49f319acc8ecd77ec082589d2b80eb
theoabraham <theo.abraham@hotmail.fr>
2023-03-11 16:57:34 +0100

    Le client doit juste écouter le serveur, et plus écouter le terminal

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+import org.json.JSONObject;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -5,7 +7,6 @@ import java.util.*;
 // Client class
 class Client {
 
-    PrintWriter out;
     BufferedReader in;
     static User user = null;
 
@@ -15,94 +16,52 @@ class Client {
         // establish a connection by providing host and port 8080
         try (Socket socket = new Socket("localhost", 8080)) {
 
-            // writing to server
-            PrintWriter out = new PrintWriter(
-                    socket.getOutputStream(), true);
-
             // reading from server
             BufferedReader in
                     = new BufferedReader(new InputStreamReader(
                     socket.getInputStream()));
 
-            getUserInfo(this.out, this.in);
+            String line;
 
-            ClientListener listener = new ClientListener(socket);
-
-            new Thread(listener).start();
-
-            // object of scanner class
-            Scanner sc = new Scanner(System.in);
-            String input = null;
-
-            while (!"exit".equalsIgnoreCase(input)) {
-                // reading from user
-                input = sc.nextLine();
-
-                // sending the user input to server
-                out.println(input);
+            while ((line = in.readLine()) != null) {
+                if (line.charAt(0) == '/') {
+                    if (user instanceof Referee) {
+                        System.out.println("Referee");
+                        //out.println(user.getLeaderboard);
+                    }
+                } else {
+                    System.out.print(line + "\n");
+                }
             }
-
-            // closing the scanner object
-            sc.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void getUserInfo(PrintWriter out, BufferedReader in) throws IOException {
-         System.out.println("Enter username");
-         Scanner input = new Scanner(System.in);
-
-         user = new User(input.nextLine());
-         out.println(user.publicKey);
-         String answer = in.readLine();
-
-         while(!answer.equals("Y")){
-             System.out.println("username already taken, please enter another one");
-             user.publicKey = input.nextLine();
-             out.println(user.publicKey);
-             answer = in.readLine();
-         }
-
-         System.out.println("Enter password");
-         String password = input.nextLine();
-         out.println(password);
+    public void sendMessage(String message, Socket socket) throws IOException {
+        JSONObject jsonObject = new JSONObject(message);
+        PrintWriter out = new PrintWriter(
+                socket.getOutputStream(), true);
+        out.println(jsonObject);
     }
 
+    public void getUserInfo(PrintWriter out, BufferedReader in) throws IOException {
+        System.out.println("Enter username");
+        Scanner input = new Scanner(System.in);
 
+        user = new User(input.nextLine());
+        out.println(user.publicKey);
+        String answer = in.readLine();
 
-    private static class ClientListener extends Thread {
-        private final Socket clientSocket;
-        PrintWriter out = null;
-        BufferedReader in = null;
-
-        public ClientListener(Socket socket) { this.clientSocket = socket;}
-        public void run() {
-            try {
-                PrintWriter out = new PrintWriter(
-                        clientSocket.getOutputStream(), true);
-
-                in = new BufferedReader(
-                        new InputStreamReader(
-                                clientSocket.getInputStream()));
-
-                String line;
-
-                while((line = in.readLine()) != null) {
-                    if (line.charAt(0) == '/') {
-                        if(user instanceof Referee) {
-                            System.out.println("Referee");
-                            //out.println(user.getLeaderboard);
-                        }
-                    } else {
-                        System.out.print(line + "\n");
-                    }
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        while (!answer.equals("Y")) {
+            System.out.println("username already taken, please enter another one");
+            user.publicKey = input.nextLine();
+            out.println(user.publicKey);
+            answer = in.readLine();
         }
+
+        System.out.println("Enter password");
+        String password = input.nextLine();
+        out.println(password);
     }
 }
