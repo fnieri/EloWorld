f4659207b8cd5680c9cb4979e449641f69e1079d
Francesco Nieri <73119651+fnieri@users.noreply.github.com>
2023-03-12 11:50:19 +0100

    test

--- src/Exceptions/UserNotInEntry.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-//https://www.javatpoint.com/custom-exception#:~:text=In%20Java%2C%20we%20can%20create,exception%20according%20to%20user%20need.
-public class UserNotInEntry extends Exception {
-    public UserNotInEntry(String message) {
-        super(message);
-    }
-}
